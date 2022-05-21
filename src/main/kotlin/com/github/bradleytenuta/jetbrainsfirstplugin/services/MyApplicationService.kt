f098c9a6f1222a0b4dfc19bf8a61c298be1e123f
GitHub Action <action@github.com>
2022-05-21 09:47:41 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bradleytenuta/jetbrainsfirstplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bradleytenuta.jetbrainsfirstplugin.services
+
+import com.github.bradleytenuta.jetbrainsfirstplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/bradleytenuta/jetbrainsfirstplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bradleytenuta/jetbrainsfirstplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.bradleytenuta.jetbrainsfirstplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bradleytenuta.jetbrainsfirstplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
